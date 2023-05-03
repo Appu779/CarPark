d889f7ba42e1d29ebf631eaa449dd1846654a44a
Sona002 <128026623+Sona002@users.noreply.github.com>
2023-05-04 00:25:17 +0530

    sidebar created

--- lib/screens/SideBar.dart
+++ lib/screens/SideBar.dart
@@ -4,29 +4,40 @@ class SideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: [
-      UserAccountsDrawerHeader(accountName: Text(''), accountEmail: Text('')),
-      ListTile(
-        leading: Icon(Icons.person),
-        title: Text('Profile'),
-        onTap: () => null,
-      ),
-      ListTile(
-        leading: Icon(Icons.settings),
-        title: Text('Settings',
-            style: TextStyle(
-              fontSize: 18,
-            )),
-        onTap: () => null,
-      ),
-      ListTile(
-        leading: Icon(Icons.arrow_back),
-        title: Text('Logout',
-            style: TextStyle(
-              fontSize: 18,
-            )),
-        onTap: () => null,
-      ),
-    ]));
+        child: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            body: Container(
+                child: ListView(padding: EdgeInsets.zero, children: [
+              UserAccountsDrawerHeader(
+                  accountName: Text(''), accountEmail: Text('')),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings',
+                    style: TextStyle(
+                      fontSize: 18,
+                    )),
+                onTap: () => null,
+              ),
+              ListTile(
+                leading: Icon(Icons.arrow_back),
+                title: Text('Logout',
+                    style: TextStyle(
+                      fontSize: 18,
+                    )),
+                onTap: () => null,
+              ),
+            ]))));
   }
 }
